e15addb5839c3ae37e0f5ef3858a6121902080c2
yuunseo <dbstj7457@naver.com>
2025-07-09 15:01:05 +0900

    main page update

--- app.js
+++ app.js
@@ -27,3 +27,33 @@ githubBtn.addEventListener('click', () => {
     </p>
   `;
 });
+
+document.querySelectorAll('.project').forEach(p => {
+  p.addEventListener('click', () => {
+    window.location.href = 'projects.html';  
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const timeline = document.querySelector('.timeline');
+  const items = document.querySelectorAll('.timeline-item');
+  const mainSection = document.getElementById('main-section');
+
+  window.addEventListener('scroll', () => {
+    const mainBottom = mainSection.getBoundingClientRect().bottom;
+    const windowHeight = window.innerHeight;
+
+    // main-section 아래를 충분히 지나가면 타임라인 애니메이션 시작
+    if (mainBottom < windowHeight * 0.5) {
+      timeline.classList.add('active');
+
+      // 각 타임라인 아이템에도 active 클래스 붙여서 개별 애니메이션 실행
+      items.forEach((item, index) => {
+        item.style.transitionDelay = `${index * 0.3 + 0.3}s`; // 0.3초 간격으로 딜레이 설정
+        item.classList.add('active');
+    });
+
+    }
+  });
+});
